fa05245a6eca878b934394eb4752cc82367c3da0
vidisodessa-sys <vidisodessa@gmail.com>
2025-08-30 14:35:10 +0100

    Update __init__.py

--- src/oir/__init__.py
+++ src/oir/__init__.py
@@ -1,1 +1,15 @@
+from .core import (
+    haar_sample_su2,
+    rotate_z_by,
+    phi,
+    oir_correlator,
+)
 
+__all__ = [
+    "haar_sample_su2",
+    "rotate_z_by",
+    "phi",
+    "oir_correlator",
+]
+
+__version__ = "0.1.0"
